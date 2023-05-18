251a347b98bd0527ee1ce08d63ca4d98860cde1f
AmirHosein <awir.software@gmail.com>
2023-05-18 13:53:48 +0330

    install redis and micro for gateway

--- gateway/src/app.controller.ts
+++ gateway/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  getHello(@Body() input: {message: string}) {
+    return this.appService.sendHello(input.message);
   }
 }
--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {Transport, ClientsModule} from "@nestjs/microservices";
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'gateway',
+        transport: Transport.REDIS,
+        options: {
+          host: 'localhost',
+          port: 6379,
+        }
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
